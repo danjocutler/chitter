30d41248c981daf49a3e575d6a10222275d1d687
danjocutler <danjocutler@gmail.com>
2014-10-16 11:40:16 +0100

    Added user sign in function and error if wrong info entered

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature "User signs up" do
+
 	scenario "when being logged out" do
 		expect {sign_up}.to change(User, :count).by 1
 		expect(page).to have_content("Welcome, testdude")
@@ -15,9 +16,34 @@ feature "User signs up" do
 
 	scenario "with an email that is already registered" do
 		expect{ sign_up }.to change(User, :count).by(1)
-    	expect{ sign_up }.to change(User, :count).by(0)
+    expect{ sign_up }.to change(User, :count).by(0)
 		expect(page).to have_content("This email is already taken")
 	end
+
+end
+
+feature "User signs in" do
+
+	before(:each) do
+		User.create(name: "TessTickle",
+								username: "testman",
+								email: "test@test.co.uk",
+								password: "test",
+								password_confirmation: "test")
+	end
+
+	scenario "with correct credentials" do
+		visit '/'
+		sign_in("test@test.co.uk", "test")
+		expect(page).to have_content("Welcome, testman")
+	end
+
+	scenario "with incorrect credentials" do
+		visit '/'
+		sign_in('test@test.co.uk', 'wrong')
+		expect(page).not_to have_content("Welcome, testman")
+	end
+end
 
 	def sign_up(name = "Tester",
 				username = "testdude",
@@ -25,11 +51,17 @@ feature "User signs up" do
 				password = "1234",
 				password_confirmation = "1234")
 		visit '/users/new'
-		fill_in :name, :with => name
-		fill_in :username, :with => username
-		fill_in :email, :with => email
-		fill_in :password, :with => password
-		fill_in :password_confirmation, :with => password_confirmation
+		fill_in :name, with: name
+		fill_in :username, with: username
+		fill_in :email, with: email
+		fill_in :password, with: password
+		fill_in :password_confirmation, with: password_confirmation
 		click_button "Sign up"
 	end
-end
+	
+	def sign_in(email, password)
+		visit '/sessions/new'
+		fill_in 'email', with: email
+		fill_in 'password', with: password
+		click_button 'Sign in'
+	end
